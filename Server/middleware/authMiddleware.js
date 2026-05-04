500f1ef4ff3e7fb2d6fc1f8940807b4f3db0f9b5
Vaishali <vv1705260@gmail.com>
2026-05-04 20:45:23 +0530

    Create authMiddleware.js

---
+++ Server/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ msg: "No token" });
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = decoded.id;
+
+  next();
+};
