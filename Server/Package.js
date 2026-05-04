5c92ede95f01ada73b874f466dc229510b814f95
Vaishali <vv1705260@gmail.com>
2026-05-04 20:38:47 +0530

    Create Package.js

---
+++ Server/Package.js
@@ -0,0 +1,21 @@
+{
+  "name": "devcollab-server",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "dev": "nodemon server.js",
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "bcryptjs": "^2.4.3",
+    "cors": "^2.8.5",
+    "dotenv": "^16.0.0",
+    "express": "^4.18.2",
+    "jsonwebtoken": "^9.0.0",
+    "mongoose": "^7.0.0",
+    "socket.io": "^4.7.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.0"
+  }
+}
