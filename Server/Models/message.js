2ac95e32b39b4b670fd8d4cadb4f0de699a4d976
Vaishali <vv1705260@gmail.com>
2026-05-04 20:42:31 +0530

    Add message model for MongoDB with Mongoose

---
+++ Server/Models/message.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  text: String,
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
